fe72f687dcf177ff79af436871ec8701aa34ca92
Jaymart <jaymart.b.araga@gmail.com>
2024-09-07 22:12:21 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,10 @@
 rootProject.name = "ShopperServer"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        google {
-            mavenContent {
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositories {
-        google {
-            mavenContent {
-
-            }
-        }
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
     }
 }
 
